d416102eff106478bc3f75354f874f5145bd2233
ionutbirlad <birladionut@yahoo.it>
2026-03-09 17:46:03 +0100

    feat: add imperative solution to problem 6

--- #6.js
+++ #6.js
@@ -27,40 +27,60 @@
 // BASELINE: PROCEDURALE (da trasformare)
 // ========================================
 
-function mergeArrays(arr1, arr2) {
-  if (!Array.isArray(arr1) || !Array.isArray(arr2)) return null;
+// function mergeArrays(arr1, arr2) {
+//   if (!Array.isArray(arr1) || !Array.isArray(arr2)) return null;
 
-  const result = [];
+//   const result = [];
 
-  for (let i = 0; i < arr1.length; i++) {
-    let found = false;
-    for (let j = 0; j < result.length; j++) {
-      if (arr1[i] === result[j]) {
-        found = true;
-        break;
-      }
-    }
-    if (!found) result.push(arr1[i]);
-  }
+//   for (let i = 0; i < arr1.length; i++) {
+//     let found = false;
+//     for (let j = 0; j < result.length; j++) {
+//       if (arr1[i] === result[j]) {
+//         found = true;
+//         break;
+//       }
+//     }
+//     if (!found) result.push(arr1[i]);
+//   }
 
-  for (let i = 0; i < arr2.length; i++) {
-    let found = false;
-    for (let j = 0; j < result.length; j++) {
-      if (arr2[i] === result[j]) {
-        found = true;
-        break;
-      }
-    }
-    if (!found) result.push(arr2[i]);
-  }
+//   for (let i = 0; i < arr2.length; i++) {
+//     let found = false;
+//     for (let j = 0; j < result.length; j++) {
+//       if (arr2[i] === result[j]) {
+//         found = true;
+//         break;
+//       }
+//     }
+//     if (!found) result.push(arr2[i]);
+//   }
 
-  return result;
-}
+//   return result;
+// }
 
 // ========================================
 // IMPERATIVO [O(n) o complessità migliore]
 // ========================================
 
+function mergeArrays(arr1, arr2) {
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) return null;
+  if (arr1.length === 0) return arr2;
+  if (arr2.length === 0) return arr1;
+
+  const rawMergedArr = [...arr1, ...arr2];
+  const result = [];
+  const seen = new Map();
+
+  for (const item of rawMergedArr) {
+    if (!seen.has(item)) {
+      seen.set(item, true);
+      result.push(item);
+    }
+  }
+
+  return result;
+}
+
+console.log(mergeArrays([1, 2], []));
 
 // ========================================
 // DICHIARATIVO [O(n) o complessità migliore]
